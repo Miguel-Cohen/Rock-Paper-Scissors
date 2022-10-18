8781f3334a9cbace7cc0fc61d4cede3b30026157
Miguel Cohen <miguelcohen17@gmail.com>
2022-10-18 11:40:44 +0100

    Add RPS assigning function.

--- script.js
+++ script.js
@@ -2,11 +2,30 @@
 
 // Initialize global variables
 let computerChoice;
+let compAssignedValue;
 
 // Make function to get computer's random choice
 function getComputerChoice(){
+    // Math.floor rounds to LOWEST instead of NEAREST integer, 
+    // which means we can eliminate the value 3 from popping up 
+    // and adding 1 afterwards to get rid of 0.
     computerChoice = Math.floor((Math.random()*3))+1;
-    console.log(computerChoice);
+    console.log(`computerChoice = ${computerChoice}`);
+    assignValues();
 }
+
+// Make function to assign an arbitrary value (either Rock, Paper or Scissors) 
+// to the computer choice's number output
+function assignValues(){
+    if(computerChoice === 1){
+        compAssignedValue = "ROCK";
+    } else if(computerChoice === 2){
+        compAssignedValue = "PAPER";        
+    } else if(computerChoice === 3){
+        compAssignedValue = "SCISSORS";
+    }
+    console.log(`compAssignedValue = ${compAssignedValue}`);
+}
+
 
 getComputerChoice();
